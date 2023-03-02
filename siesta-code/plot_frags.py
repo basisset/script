b8d76eeefd3ff8e22e839249ba6bd7773edad0de
basisset <pamelasvensson@gmail.com>
2023-03-02 11:14:30 +0100

    Minor changes

--- siesta-code/plot_frags.py
+++ siesta-code/plot_frags.py
@@ -5,8 +5,8 @@ import numpy as np
 import mass_spec as ms
 import matplotlib.pyplot as plt
 
-job = 'MetINim-z4'
-parent_mass = 254
+job = 'Nim-z3'
+parent_mass = 128  #metINim 254 Nim 128
 nr_runs = 20
 
 with open('fragments.pickle','rb') as f:
@@ -22,30 +22,35 @@ for geo in range(len(fragments)):
                 element = ''.join([i for i in fragments[geo][ion][frag][atom] if not i.isdigit()])
                 mol.append(element) #get atom to molecule
             frag_mass = ms.get_mass(mol)
-
+            if frag_mass == 99:
+                print(mol)
             frag_data.append(frag_mass)
+
 arr_data = np.array(frag_data)
-hist = np.histogram(arr_data,bins=int(np.max(arr_data))-1)
+count = np.zeros(parent_mass+1)
+for i in range(parent_mass+1):
+    count[i]=frag_data.count(i)
+
+#h,b,p = plt.hist(arr_data, bins=255)
+#bins = np.linspace(0, parent_mass+1, 1000)
+#count, bins = np.histogram(arr_data, bins=bins)
+#print(bins[0], bins[1])
+#mass = 0.5*(bins[1:] + bins[:-1])
 #To start masspec at 0
-mass = np.insert(hist[1],0,0)
-count = np.append(hist[0],0)
-count = np.insert(count,0,0)
 percent = count/nr_runs*100
 
-#print(hist[0])
-#print(count)
-fig,ax = plt.subplots()
-
+#fig,ax = plt.subplots()
 with open(job+'-fragment_data.txt','w') as f:
     f.write('Fragments obtained from Siesta molecular dynamics for system '+job+'\n')
     f.write('Mass (m/z)\tIntensity\tPercent\n')
-    for i in range(mass.size):
+    for i in range(count.size):
         if count[i] > 0 :
-            f.write(f'{mass[i]:>5}\t{count[i]:>10}\t{percent[i]:>10}\n')
-ax.plot(mass,count)
-ax.set_xlabel('m/z')
-ax.set_ylabel('intensity (arb.u.)')
-plt.savefig(job+'-mass_spec.png')
-plt.show()
+            f.write(f'{i:>5}\t{count[i]:>10}\t{percent[i]:>10}\n')
+    print('File saved!') 
+#ax.plot(mass,count)
+#ax.set_xlabel('m/z')
+#ax.set_ylabel('intensity (arb.u.)')
+#plt.savefig(job+'-mass_spec.png')
+#plt.show()
 
 
